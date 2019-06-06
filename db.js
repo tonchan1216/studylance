e18821f7a5ab372edb204bce876cd954cc686b72
tonchan1216 <10722536+tonchan1216@users.noreply.github.com>
2019-06-06 10:53:17 +0900

    認証情報削除

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 //Database
 var pg = require('pg');
-var connectionString = "tcp://postgres://jrvenknemncwiw:e30025c1205881535893b0a93dd41a2b6e4da3e17f425066f31484f2c566489f@ec2-54-243-39-245.compute-1.amazonaws.com:5432/d9dkdotlj74l6o";
+var connectionString = "AWS";
 
 var client = new pg.Client(connectionString);
 client.connect();
